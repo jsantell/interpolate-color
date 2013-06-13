709fde8d9c334d9b3483793cc24b0e4508c64813
Jordan Santell <jsantell@gmail.com>
2013-06-12 23:15:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# interpolate-color
+
+[view example](http://jsantell.github.io/interpolate-color)
+
+A simple color interpolator component
+
+## Installation
+
+    $ component install jsantell/interpolate-color
+
+## Example Usage
+
+```javascript
+var interpolate = require('interpolate-color');
+var red = 'hsl(0, 100%, 50%)';
+var green = 'hsl(120, 100%, 50%)';
+
+interpolate(red, blue, 0.5); // 'hsl(60, 100%, 50%)'
+```
+
+## API
+
+### interpolate(from, to, step, [precision])
+
+- `from` the starting position HSL string (`hsl(0, 100%, 50%)`)
+- `to` the end position HSL string
+- `step` the normalized value (between 0 and 1) of the interpolation. A step of `0.5` would be the middle of `from` and `to`
+- `precision` an optional argument of how many points of precision the results should have (default: `0`)
+
+
+## Tests
+
+View `./test/index.html`
+
+## License
+
+MIT License
---
+++ test/test.js
@@ -0,0 +1,34 @@
+var i = require('interpolate-color');
+
+var green = 'hsl(120, 100%, 50%)';
+var red = 'hsl(0, 100%, 50%)';
+var blue = 'hsl(240, 100%, 50%)';
+
+describe('Color Interpolator', function () {
+  it('has correct interpolation', function () {
+    expect(i(green, red, 0.5)).to.equal('hsl(60, 100%, 50%)');
+    expect(i(green, red, 0)).to.equal(green);
+    expect(i(green, red, 1)).to.equal(red);
+    expect(i(blue, red, 0.75)).to.equal('hsl(60, 100%, 50%)');
+
+    expect(i('hsl(25, 0%, 100%)', 'hsl(125, 100%, 0%)', 0.5)).to.equal(
+      'hsl(75, 50%, 50%)'
+    );
+  });
+
+  it('ignores extra spaces', function () {
+    expect(i('hsl(25,0%,100%)', 'hsl(  125,  100% , 0% )', 0.5)).to.equal(
+      'hsl(75, 50%, 50%)'
+    );
+  });
+
+  it('accepts precision values', function () {
+    expect(i(green, red, 1, 2)).to.equal('hsl(0.00, 100.00%, 50.00%)');
+  });
+
+  it('accepts input with precision', function () {
+    expect(i('hsl(120.00, 100.00%, 50.00%)', 'hsl(0.00, 100.00%, 50.00%)', 0.5)).to.equal(
+      'hsl(60, 100%, 50%)'
+    );
+  });
+});
